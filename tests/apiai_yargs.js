ac7823c2858640dc244717077bb527067e27f488
Kelly <kgilliamcode@gmail.com>
2018-03-03 17:53:40 -0800

    [feature] Make basic open ended conversation interface

--- tests/apiai_yargs.js
+++ tests/apiai_yargs.js
@@ -5,15 +5,16 @@ var yargs = require('yargs');
 var app = apiai("your_api_key");
 
 var request = app.textRequest(yargs.argv._[0], {
-    sessionId: '<unique session id>'
+	sessionId: '<unique session id>'
 });
 
 request.on('response', function(response) {
-    console.log(response.result.fulfillment.speech);
+	console.log(response.result.fulfillment.speech);
 });
 
 request.on('error', function(error) {
-    console.log(error);
+	console.log(error);
 });
 
 request.end();
+
